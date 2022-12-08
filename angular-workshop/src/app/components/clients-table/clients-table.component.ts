66d543b8bf398ea292a8d922b737be8a8a1fc6af
romanovich_anton <anton.romanovich21@gmail.com>
2022-12-08 14:32:59 +0300

    feat: get data for table from fake server

--- angular-workshop/src/app/components/clients-table/clients-table.component.ts
+++ angular-workshop/src/app/components/clients-table/clients-table.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client-service.service';
+import { IClient } from 'src/app/types/interfaces';
 
 @Component({
   selector: 'app-clients-table',
   templateUrl: './clients-table.component.html',
   styleUrls: ['./clients-table.component.scss']
 })
-export class ClientsTableComponent {
+export class ClientsTableComponent implements OnInit{
+  clients: [] | IClient[]
+  constructor(private ClientService: ClientService) {}
 
+  ngOnInit(): void {
+    this.ClientService.getClients()
+      .subscribe((data: IClient[]) => this.clients = data)
+  }
 }
